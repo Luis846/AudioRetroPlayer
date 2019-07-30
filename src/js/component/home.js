3de9a30c5d5ad2a57f515f956c5158c4169dcf9f
Luis Rivera <luis.rivera846@gmail.com>
2019-07-30 14:16:12 +0000

    added

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,9 +1,5 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export class Home extends React.Component {
 	constructor() {
 		super();
@@ -29,46 +25,58 @@ export class Home extends React.Component {
 
 	render() {
 		return (
-			<div className="container">
-				<div className="">
+			<div>
+				<div className="container">
+					<span className="title-retro"> Audio Player Retro</span>
+
 					<div>
-						{this.state.songs ? (
-							this.state.songs.map((item, index) => {
-								return (
-									<div
-										key={index}
-										onClick={e =>
-											this.setState({
-												selectedIndex: index
-											})
-										}
-										className="bg-dark border text-white p-2 outers">
-										<span className="text-secondary">
-											{index}
-										</span>
-										<span
-											onClick={() => this.audioActive()}>
-											{item.name} -{" "}
-										</span>
-										{this.state.selectedIndex === index && (
-											<audio
-												type="audio/mp3"
-												controls
-												src={
-													"https://assets.breatheco.de/apis/sound/" +
-													item.url
-												}
-												onClick={e =>
-													this.audioClick(e)
-												}
-											/>
-										)}
-									</div>
-								);
-							})
-						) : (
-							<div>Loading</div>
-						)}
+						<div className="">
+							{this.state.songs ? (
+								this.state.songs.map((item, index) => {
+									return (
+										<div
+											key={index}
+											onClick={e =>
+												this.setState({
+													selectedIndex: index
+												})
+											}
+											className="bg-dark border text-white p-2 outers">
+											<span
+												className="text-secondary"
+												id="number">
+												{index}
+											</span>
+											<span
+												className="pointed"
+												onClick={() =>
+													this.audioActive()
+												}>
+												{item.name}{" "}
+											</span>
+											{this.state.selectedIndex ===
+												index && (
+												<audio
+													controls
+													className="float-right"
+													autoPlay
+													type="audio/mp3"
+													src={
+														"https://assets.breatheco.de/apis/sound/" +
+														item.url
+													}
+													onClick={e =>
+														this.audioClick(e)
+													}
+												/>
+											)}
+										</div>
+									);
+								})
+							) : (
+								<div>Loading</div>
+							)}
+						</div>
 					</div>
 				</div>
 			</div>
